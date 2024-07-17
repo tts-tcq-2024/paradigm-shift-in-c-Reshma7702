cde6e84eb9e8df07cf94b67c785b77a5bf15d14c
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-17 21:50:12 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -33,27 +33,33 @@ int performCheck(const Check* check) {
     return 1;
 }
 
+int checkAllParameters(const Check checks[], int numChecks) {
+    for (int i = 0; i < numChecks; ++i) {
+        if (!performCheck(&checks[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     Check checks[] = {
         {isTemperatureInRange, temperature, "Temperature out of range!\n"},
         {isSocInRange, soc, "State of Charge out of range!\n"},
         {isChargeRateInRange, chargeRate, "Charge Rate out of range!\n"}
     };
-
-    for (int i = 0; i < sizeof(checks) / sizeof(checks[0]); ++i) {
-        if (!performCheck(&checks[i])) {
-            return 0;
-        }
-    }
-
-    return 1;
+    return checkAllParameters(checks, sizeof(checks) / sizeof(checks[0]));
 }
 
-int main() {
+void runTests() {
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
     assert(!batteryIsOk(30, 85, 0));
     assert(!batteryIsOk(25, 70, 0.9));
     printf("All tests passed!\n");
+}
+
+int main() {
+    runTests();
     return 0;
 }
