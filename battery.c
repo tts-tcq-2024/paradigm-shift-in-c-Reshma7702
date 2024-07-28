00c141a59954eb51cd30764cc1dd2c095293a2cd
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-28 11:35:09 +0530

    Create battery.c

---
+++ battery.c
@@ -0,0 +1,18 @@
+#include "battery.h"
+#include "check.h"
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+    Check checks[] = {
+        {isTemperatureInRange, temperature, 2.25, "Temperature out of range!\n", "Warning: Approaching low temperature limit!\n", "Warning: Approaching high temperature limit!\n"},
+        {isSocInRange, soc, 4.0, "State of Charge out of range!\n", "Warning: Approaching low SoC limit!\n", "Warning: Approaching high SoC limit!\n"},
+        {isChargeRateInRange, chargeRate, 0.04, "Charge Rate out of range!\n", "", "Warning: Approaching high charge rate limit!\n"}
+    };
+
+    for (int i = 0; i < sizeof(checks) / sizeof(checks[0]); ++i) {
+        if (!performCheck(&checks[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
