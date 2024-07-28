f8684e7ec46ff61973296ebf59512d749698ae09
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-28 11:51:17 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,6 +7,7 @@ int main() {
     assert(!batteryIsOk(50, 85, 0));
     assert(!batteryIsOk(30, 85, 0));
     assert(!batteryIsOk(25, 70, 0.9));
+    assert(!batteryIsOk(2.3, 70, 0.9));
     printf("All tests passed!\n");
     return 0;
 }
