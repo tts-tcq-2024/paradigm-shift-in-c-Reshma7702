144d08af887633a74bb76bed1571e1fb5dc98421
Reshma7702 <reshmabuddy7702@gmail.com>
2024-07-28 11:44:45 +0530

    Update battery.c

--- battery.c
+++ battery.c
@@ -5,7 +5,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
     Check checks[] = {
         {isTemperatureInRange, temperature, 2.25, "Temperature out of range!\n", "Warning: Approaching low temperature limit!\n", "Warning: Approaching high temperature limit!\n"},
         {isSocInRange, soc, 4.0, "State of Charge out of range!\n", "Warning: Approaching low SoC limit!\n", "Warning: Approaching high SoC limit!\n"},
-        {isChargeRateInRange, chargeRate, 0.04, "Charge Rate out of range!\n", "", "Warning: Approaching high charge rate limit!\n"}
+        {isChargeRateInRange, chargeRate, 0.04, "Charge Rate out of range!\n", NULL, "Warning: Approaching high charge rate limit!\n"}
     };
 
     for (int i = 0; i < sizeof(checks) / sizeof(checks[0]); ++i) {
