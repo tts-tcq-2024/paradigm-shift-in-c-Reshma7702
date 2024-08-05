1e72de887959db935145bba652239fc06c3526e6
Reshma7702 <reshmabuddy7702@gmail.com>
2024-08-05 22:26:16 +0530

    Update battery_monitor.c

--- battery_monitor.c
+++ battery_monitor.c
@@ -2,9 +2,9 @@
 #include "parameter_checks.h"
 #include <stddef.h>
 
-int performAllChecks(Check checks[], float minLimits[], float maxLimits[], int size) {
+int performAllChecks(ParameterCheck parameterChecks[], float minLimits[], float maxLimits[], int size) {
     for (int i = 0; i < size; ++i) {
-        if (performCheck(&checks[i], minLimits[i], maxLimits[i]) == 0) {
+        if (performCheck(&parameterChecks[i], minLimits[i], maxLimits[i]) == 0) {
             return 0;
         }
     }
@@ -12,7 +12,7 @@ int performAllChecks(Check checks[], float minLimits[], float maxLimits[], int s
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-    Check checks[] = {
+    ParameterCheck parameterChecks[] = {
         {isTemperatureInRange, temperature, 2.25, "Temperature out of range!\n", "Warning: Approaching low temperature limit!\n", "Warning: Approaching high temperature limit!\n"},
         {isSocInRange, soc, 4.0, "State of Charge out of range!\n", "Warning: Approaching low SoC limit!\n", "Warning: Approaching high SoC limit!\n"},
         {isChargeRateInRange, chargeRate, 0.04, "Charge Rate out of range!\n", NULL, "Warning: Approaching high charge rate limit!\n"}
@@ -21,5 +21,5 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
     float minLimits[] = {0, 20, 0};
     float maxLimits[] = {45, 80, 0.8};
 
-    return performAllChecks(checks, minLimits, maxLimits, sizeof(checks) / sizeof(checks[0]));
+    return performAllChecks(parameterChecks, minLimits, maxLimits, sizeof(parameterChecks) / sizeof(parameterChecks[0]));
 }
