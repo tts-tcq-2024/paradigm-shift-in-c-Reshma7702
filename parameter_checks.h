09ec7617ae60070f5f4d57fe69f2fe7928cc86f8
Reshma7702 <reshmabuddy7702@gmail.com>
2024-08-05 22:00:28 +0530

    Update parameter_checks.h

--- parameter_checks.h
+++ parameter_checks.h
@@ -13,12 +13,7 @@ typedef struct {
 } Check;
 
 void printMessage(const char *message);
-int isTemperatureInRange(float temperature);
-int isSocInRange(float soc);
-int isChargeRateInRange(float chargeRate);
 int performCheck(const Check* check, float minLimit, float maxLimit);
-void checkLowWarning(const Check* check, float minLimit);
-void checkHighWarning(const Check* check, float maxLimit);
 
 #endif // CHECK_H
 
